1a382d3b533c033d16f36a2cce157a2834cee724
“davidcampbell97” <davidcampbell97@hotmail.com>
2021-11-08 15:20:35 +0000

    seperated conversion functionality to seperate method

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -7,23 +7,32 @@ class Dec2Hex
 
     public static void main(String args[])
     {
-        Arg1 = Integer.parseInt(args[0]);
-        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-        int rem, num;
-        num = Arg1;
-        String hexadecimal="";
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
-
-        while(num != 0)
-        {
-            rem=num%16;
-            hexadecimal= ch[rem] + hexadecimal;
-            num= num/16;
-        }
-
-        System.out.println("Hexadecimal representation is: " + hexadecimal);
-	System.out.println("Hello!");
-
+	calculateHex(args[0]);
     }
-}
 
+
+
+
+
+
+  public static String calculateHex(String Arg1)
+            {
+
+                char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+                int rem, num;
+                num = Integer.parseInt(Arg1);
+
+                String hexadecimal="";
+                System.out.println("Converting the Decimal Value " + num + " to Hex...");
+
+                while(num != 0)
+                {
+                    rem=num%16;
+                    hexadecimal= ch[rem] + hexadecimal;
+                    num= num/16;
+                }
+
+                System.out.println("Hexadecimal representation is: " + hexadecimal);
+                return hexadecimal;
+            }
+}
